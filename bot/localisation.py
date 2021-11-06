cf7edbf216e274ca65f026c0ace43a0af6dd7159
OMINDA DILSHAN RESPACT <85678266+omindadelshan@users.noreply.github.com>
2021-11-06 14:00:11 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -47,7 +47,7 @@ class Localisation:
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @slbotzone 🎃"
+        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @MAC_Chat_G 🎃"
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
